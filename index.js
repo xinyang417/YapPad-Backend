112a5a7a73c740c9cdf0c9a39cd1213f168206f5
Simrat <simrats169169@gmail.com>
2024-03-15 16:12:17 -0700

    Initial express app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const PORT = 8000
+
+app.get('/', (req, res) => {
+    return res.send("Hello World!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Started listening on port ${PORT}`)
+})
