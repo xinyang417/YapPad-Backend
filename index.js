3146e75b2e55e4e3358559d50896603d19458b82
Simrat <simrats169169@gmail.com>
2024-03-25 20:27:56 -0700

    db: Fix mongo connection
    Switch to mongoose instead of the default mongo connection

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require("express");
-const { dbConnect } = require("./services/db_service");
+const auth_router = require("./routes/auth");
+const DbService = require("./services/db_service")
 
 const app = express();
 const PORT = 8000;
+
+app.use(express.json());
+app.use("/auth", auth_router);
 
 app.get("/", (req, res) => {
   return res.send("Hello World!");
 });
 
 async function main() {
-  await dbConnect().catch((e) => {
-    console.error("An error occured while connecting to mongodb");
-    console.error(e);
-    process.exit(1);
-  });
-
-  app.listen(PORT, () => {
-    console.log(`Started listening on port ${PORT}`);
-  });
+  const db = new DbService()
+  db.connect()
+    .catch((e) => {
+      console.error("An error occured while connecting to mongodb");
+      console.error(e);
+      process.exit(1);
+    })
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log(`Started listening on port ${PORT}`);
+      });
+    });
 }
 
 main();
--- services/db_service.js
+++ services/db_service.js
@@ -1,35 +1,27 @@
 const mongoose = require("mongoose");
 
-/**
- * Connect to the database
- * @returns {Promise<mongoose.Connection>} An object representing a connection to the database
- */
-async function dbConnect() {
-  const { MongoClient, ServerApiVersion } = require("mongodb");
-  const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/YapPad";
+class DbService {
+  constructor() {
+    if (DbService._instance) {
+      return DbService._instance;
+    }
 
-  // Create a MongoClient
-  const client = new MongoClient(MONGO_URL, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
+    DbService._instance = this;
+  }
 
-  try {
-    // Connect the client to the server
-    await client.connect();
-    console.log("Connected to MongoDB!");
+  async connect() {
+    const MONGO_URL =
+      process.env.MONGO_URL || "mongodb://127.0.0.1:27017/YapPad";
 
-    // Return the mongoose connection from the client
-    return client.db();
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    throw error;
+    try {
+      // Connect the client to the server
+      await mongoose.connect(MONGO_URL);
+      console.log("Connected to MongoDB!");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      throw error;
+    }
   }
 }
 
-module.exports = {
-  dbConnect,
-};
+module.exports = DbService;
