4bcfcd7047e75a97344692ccab53eb270311009a
Gathrean Dela Cruz <gathrean@icloud.com>
2024-03-28 09:46:16 -0700

    Update file paths and add CORS configuration

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
-const auth_router = require("./routes/auth_route");
-const DbService = require("./services/db_service");
+const auth_router = require("./routes/auth_route.js");
+const DbService = require("./services/db_service.js");
 
 const app = express();
 const PORT = 8000;
 
+// Enable CORS for all routes
+const cors = require("cors");
+app.use(cors());
+
+// Parse JSON requests
 app.use(express.json());
+
+// Define routes
 app.use("/auth", auth_router);
+
+// Additional CORS configuration if needed
+app.use(cors({
+  origin: 'http://localhost:5173/' // Allow requests from this origin only
+}));
 
 app.get("/", (req, res) => {
   return res.send("Hello World!");
@@ -16,7 +28,7 @@ async function main() {
   const db = new DbService();
   db.connect()
     .catch((e) => {
-      console.error("An error occured while connecting to mongodb");
+      console.error("An error occurred while connecting to MongoDB");
       console.error(e);
       process.exit(1);
     })
