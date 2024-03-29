c79152014e773941e594d26f36ef6c07a955346c
Simrat <simrats169169@gmail.com>
2024-03-28 22:08:44 -0700

    admin: Add endpoint to get all users

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const admin_check_middleware = require("../middleware/admin_check");
+const User = require("../models/user_model");
 
 const router = express.Router();
 
-router.get("/users", admin_check_middleware, async (req, res, next) => {
-  res.json({});
+router.get("/users", admin_check_middleware, async (req, res) => {
+  const all_users = await User.find()
+  all_users.forEach((u) => {
+    u.password = undefined
+  })
+
+  res.json(all_users);
 });
 
 module.exports = router;
