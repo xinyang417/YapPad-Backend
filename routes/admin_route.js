9ce057ea04792a0e163f66c600e4d8f9a9d1ebef
Simrat Grewal <simrats169169@gmail.com>
2024-03-29 21:55:09 -0700

    admin: Send api consumption

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const admin_check_middleware = require("../middleware/admin_check");
 const User = require("../models/user_model");
+const { getApiConsumption } = require("../services/api_consumption_service");
 
 const router = express.Router();
 
 router.get("/users", admin_check_middleware, async (req, res) => {
   const all_users = await User.find()
-  all_users.forEach((u) => {
-    u.password = undefined
-  })
+  const ret_users = await Promise.all(all_users.map(async (u) => {
+    const consumption = await getApiConsumption(u.id)
+    return { _id: u.id, username: u.username, email: u.email, isAdmin: u.isAdmin, consumption: consumption}
+  }))
 
-  res.json(all_users);
+  res.json(ret_users);
 });
 
 module.exports = router;
