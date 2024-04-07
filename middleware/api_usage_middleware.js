c5e0a80cebca63e5c132dedfd0073a185a21fba3
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2024-04-06 21:36:09 -0700

    /update

--- middleware/api_usage_middleware.js
+++ middleware/api_usage_middleware.js
@@ -1,25 +1,19 @@
 const ApiUsage = require("../models/api_usage_model");
 
 async function incrementEndpointUsage(req, _, next) {
-  let method = req.method;
-  let endpoint = req.originalUrl; 
-
+  const method = req.method;
+  let endpoint = req.originalUrl;
   console.log(`Original endpoint: ${endpoint}`); 
 
-  // normalize delete endpoint for yap deletion
-  if (req.method === 'DELETE' && endpoint.startsWith('/v1/yaps/delete/')) {
-    // normalize the endpoint for all delete operations to be tracked under a single record
-    // or else u get a new inserted record for every unique yap deleted!!!
-    endpoint = '/v1/yaps/delete'; // reassignment now affects endpoint variable 
+// normalize endpoint for PUT and DELETE requests
+// normalize the endpoint for all delete operations to be tracked under a single record
+// or else u get a new inserted record for every unique yap deleted!!!
+  if ((method === 'PUT' && endpoint.startsWith('/v1/yaps/update/')) ||
+      (method === 'DELETE' && endpoint.startsWith('/v1/yaps/delete/'))) {
+    endpoint = `/v1/yaps/${method === 'PUT' ? 'update' : 'delete'}`;
     console.log(`Normalized endpoint for DELETE: ${endpoint}`);
-}
-
-
- // normalize put endpoint for all yap updates
- if (req.method === 'PUT' && endpoint.startsWith('/v1/yaps/update/')) {
-    endpoint = '/v1/yaps/update'; 
+    console.log(`Normalized endpoint: ${endpoint}`);
   }
-
 
   try {
     const usage = await ApiUsage.findOneAndUpdate(
